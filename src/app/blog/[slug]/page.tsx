38acd4ce3308e7067bd77107f33b312d45ba24ee
Adrian Ross <adriantoddross@gmail.com>
2024-05-14 14:46:56 -0700

    Test slug on blog page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <main>Hello, world!</main>;
+export default function Home({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+
+  return (
+    <main>
+      <h1>Hello, world!</h1>
+      <p>Slug: {slug}</p>
+    </main>
+  );
+
+  async function getBlogPost() {
+    "use server";
+    // get slug from context
+    // import gray matter config
+    // return props: title, frontmatter and markdown
+  }
 }
