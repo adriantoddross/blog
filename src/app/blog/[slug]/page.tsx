803483258a93190a004240d63d6c1ea022cce3c5
Adrian Ross <adriantoddross@gmail.com>
2024-05-30 15:05:57 -0700

    Generate shortened file path for posts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -5,8 +5,7 @@ import { readdirSync, lstatSync } from "fs";
 import { notFound } from "next/navigation";
 
 export async function generateStaticParams() {
-  const blogPostsPath = path.resolve(process.cwd(), "./src/posts/");
-  const slugs = generatePostSlugs(blogPostsPath);
+  const slugs = generateBlogPostUrls(blogPostsPath);
 
   return slugs.map((slug: string) => {
     return { slug };
@@ -16,17 +15,12 @@ export async function generateStaticParams() {
 export default async function Home({ params }: { params: { slug: string } }) {
   // guide: https://tina.io/blog/simple-markdown-blog-nextjs/
 
+  const blogPostPaths = generateBlogsMap(blogPostsPath);
+  console.log("generateBlogsMap", blogPostPaths.get(params.slug));
+
   const { slug } = params;
   const { frontmatter, markdownBody } = await getMarkdownFile(slug);
   const { author, date, tags, title } = frontmatter;
-
-  const blogPostsPath = path.resolve(process.cwd(), "./src/posts/");
-  const postSlugs = generatePostSlugs(blogPostsPath);
-  console.log(
-    postSlugs.map((name: string) => {
-      return { slug: name };
-    })
-  );
 
   return (
     <main>
@@ -40,9 +34,12 @@ export default async function Home({ params }: { params: { slug: string } }) {
   );
 }
 
-async function getMarkdownFile(blogSlug: string) {
+async function getMarkdownFile(slug: string) {
   try {
-    const content = await import(`../../../posts/2024/${blogSlug}.md`);
+    // const blogPostPaths = generateBlogsMap(blogPostsPath);
+    // const content = await import(`${blogPostPaths.get(slug)}`);
+    const content = await import(`../../../posts/2024/${slug}.md`);
+
     const data = matter(content.default);
 
     return {
@@ -54,17 +51,40 @@ async function getMarkdownFile(blogSlug: string) {
   }
 }
 
-function generatePostSlugs(folderPath: string) {
-  function isFile(fileName: string) {
-    return lstatSync(fileName).isFile();
-  }
+const blogPostsPath = path.resolve(process.cwd(), "./src/posts/");
 
+function isFile(fileName: string) {
+  return lstatSync(fileName).isFile();
+}
+
+function formatPathAsUrl(filePath: string, fileType: string) {
+  return path.basename(filePath, fileType).replace(/ /g, "-");
+}
+
+function getBlogPosts(folderPath: string) {
   return readdirSync(folderPath, { recursive: true })
     .map((fileName: string | Buffer) => {
       return path.join(folderPath, fileName.toString());
     })
-    .filter(isFile)
-    .map((filePath: string) => {
-      return path.basename(filePath, ".md").replace(/ /g, "-");
-    });
+    .filter(isFile);
+}
+
+function generateBlogPostUrls(folderPath: string) {
+  return getBlogPosts(folderPath).map((filePath: string) => {
+    return formatPathAsUrl(filePath, ".md").replace(/ /g, "-");
+  });
+}
+
+function generateBlogsMap(folderPath: string) {
+  const blogsMap: Map<string, string> = new Map();
+  const blogPostPaths = getBlogPosts(folderPath);
+
+  blogPostPaths.forEach((filePath) => {
+    blogsMap.set(
+      formatPathAsUrl(filePath, ".md"),
+      path.relative(folderPath, filePath.toString())
+    );
+  });
+
+  return blogsMap;
 }
