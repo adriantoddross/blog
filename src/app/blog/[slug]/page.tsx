19a0fb88662eb18143cf294c4f5adceefdca5570
Adrian Ross <adriantoddross@gmail.com>
2024-05-30 11:28:58 -0700

    Get all md files

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,17 +1,15 @@
 import matter from "gray-matter";
 import ReactMarkdown from "react-markdown";
 import path from "path";
-import { readdirSync } from "fs";
+import { readdirSync, lstatSync } from "fs";
 
 export default async function Home({ params }: { params: { slug: string } }) {
   const { slug } = params;
   const { frontmatter, markdownBody } = await getMarkdownFile(slug);
   const { author, date, tags, title } = frontmatter;
 
-  const blogPostsPath = path.resolve(process.cwd(), "./src/posts/2024");
-  const files = readdirSync(blogPostsPath, { recursive: true });
-
-  console.log(files);
+  const blogPostsPath = path.resolve(process.cwd(), "./src/posts/");
+  console.log(getFileNames(blogPostsPath));
 
   return (
     <main>
@@ -28,13 +26,31 @@ export default async function Home({ params }: { params: { slug: string } }) {
     const content = await import(`../../../posts/2024/${blogSlug}.md`);
     const data = matter(content.default);
 
+    // TODO: throw 404 page for bad slug
+
     return {
       frontmatter: data.data,
       markdownBody: data.content,
     };
   }
 
-  function getFileNames() {}
+  function getFileNames(folderPath: string) {
+    function isFile(fileName: string) {
+      return lstatSync(fileName).isFile();
+    }
+
+    return readdirSync(folderPath, { recursive: true })
+      .map((fileName: string | Buffer) => {
+        return path.join(folderPath, fileName.toString());
+      })
+      .filter(isFile)
+      .map((filePath: string) => {
+        return path.basename(filePath);
+        // TODO: format the filenames into URL
+      });
+
+    // return array of strings
+  }
 }
 
 // https://tina.io/blog/simple-markdown-blog-nextjs/
