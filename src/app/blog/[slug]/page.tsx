4d2d2a6245a97bb5ca0129b926d21445c84f3ef8
Adrian Ross <adriantoddross@gmail.com>
2024-05-30 12:42:30 -0700

    generateStaticParams for SSG

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,13 +3,29 @@ import ReactMarkdown from "react-markdown";
 import path from "path";
 import { readdirSync, lstatSync } from "fs";
 
+export async function generateStaticParams() {
+  const blogPostsPath = path.resolve(process.cwd(), "./src/posts/");
+  const slugs = generatePostSlugs(blogPostsPath);
+
+  return slugs.map((slug: string) => {
+    return { slug };
+  });
+}
+
 export default async function Home({ params }: { params: { slug: string } }) {
+  // guide: https://tina.io/blog/simple-markdown-blog-nextjs/
+
   const { slug } = params;
   const { frontmatter, markdownBody } = await getMarkdownFile(slug);
   const { author, date, tags, title } = frontmatter;
 
   const blogPostsPath = path.resolve(process.cwd(), "./src/posts/");
-  console.log(getFileNames(blogPostsPath));
+  const postSlugs = generatePostSlugs(blogPostsPath);
+  console.log(
+    postSlugs.map((name: string) => {
+      return { slug: name };
+    })
+  );
 
   return (
     <main>
@@ -21,36 +37,31 @@ export default async function Home({ params }: { params: { slug: string } }) {
       <ReactMarkdown>{markdownBody}</ReactMarkdown>
     </main>
   );
-
-  async function getMarkdownFile(blogSlug: string) {
-    const content = await import(`../../../posts/2024/${blogSlug}.md`);
-    const data = matter(content.default);
-
-    // TODO: throw 404 page for bad slug
-
-    return {
-      frontmatter: data.data,
-      markdownBody: data.content,
-    };
-  }
-
-  function getFileNames(folderPath: string) {
-    function isFile(fileName: string) {
-      return lstatSync(fileName).isFile();
-    }
-
-    return readdirSync(folderPath, { recursive: true })
-      .map((fileName: string | Buffer) => {
-        return path.join(folderPath, fileName.toString());
-      })
-      .filter(isFile)
-      .map((filePath: string) => {
-        return path.basename(filePath);
-        // TODO: format the filenames into URL
-      });
-
-    // return array of strings
-  }
 }
 
-// https://tina.io/blog/simple-markdown-blog-nextjs/
+async function getMarkdownFile(blogSlug: string) {
+  const content = await import(`../../../posts/2024/${blogSlug}.md`);
+  const data = matter(content.default);
+
+  // TODO: throw 404 page for bad slug
+
+  return {
+    frontmatter: data.data,
+    markdownBody: data.content,
+  };
+}
+
+function generatePostSlugs(folderPath: string) {
+  function isFile(fileName: string) {
+    return lstatSync(fileName).isFile();
+  }
+
+  return readdirSync(folderPath, { recursive: true })
+    .map((fileName: string | Buffer) => {
+      return path.join(folderPath, fileName.toString());
+    })
+    .filter(isFile)
+    .map((filePath: string) => {
+      return path.basename(filePath, ".md").replace(/ /g, "-");
+    });
+}
